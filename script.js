e68dd6541c1e21f34581453fa48343bc0e75f09e
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2023-06-01 16:16:35 +0400

    faq-accordion-card

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const lists = document.querySelectorAll(".lists");
-const hide = document.querySelectorAll(".hide");
 for(let i of lists) {
     i.addEventListener('click', (event) => {
         const target = event.currentTarget.nextElementSibling;
-        const targetImg = event.target;
         if (target.style.display === 'block') {
             target.style.display = 'none';
-            targetImg.style.fontWeight = '400';
-            targetImg.nextElementSibling.style.transform = "rotate(0)";
+            i.childNodes[1].style.fontWeight = '400';
+            i.childNodes[3].style.transform = "rotate(0)";
             return;
           }
             target.style.display = 'block';
-            targetImg.style.fontWeight = '700';
-            targetImg.nextElementSibling.style.transform = "rotate(180deg)";
-    })
+            i.childNodes[1].style.fontWeight = '700';
+            i.childNodes[3].style.transform = "rotate(180deg)";
+    });
 };
